6ffc95cc205d0e8991047bb09c571d29487976d6
MuZakkir Saifi <mohd.saifi@knoldus.com>
2022-08-28 10:12:41 +0530

    Update in code

--- modify_security_grp.py
+++ modify_security_grp.py
@@ -1,3 +1,4 @@
+# MuZakkir Saifi
 # import logging for get the logs in  execution
 import logging
 # import the boto3 which will use to interact  with the aws
@@ -5,48 +6,47 @@ import boto3
 from botocore.exceptions import ClientError
 import json
 
-AWS_REGION = input("Please enter the AWS_REGION")
+REGION = input("Please enter the REGION: ")
 
-# this is the configration for the logger
+# this is the configration for the logger_for
 
-logger = logging.getLogger()
+logger_for = logging.getLogger()
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s: %(levelname)s: %(message)s')
 
-vpc_client = boto3.client("ec2", region_name=AWS_REGION)
+client_for_grp = boto3.client("ec2", region_name=REGION)
 
 
-def rule_modification(security_group_id, security_group_rule_id):
+def rule_modification(grp_id, grp_rule_id):
     
     try:
-        response = vpc_client.modify_security_group_rules(
-            GroupId=security_group_id,
+        res = client_for_grp.modify_security_group_rules(
+            GroupId=grp_id,
             SecurityGroupRules=[
                 {
-                    'SecurityGroupRuleId': security_group_rule_id,
+                    'SecurityGroupRuleId': grp_rule_id,
                     'SecurityGroupRule': {
                         'IpProtocol': 'tcp',
                         'FromPort': 8080,
                         'ToPort': 8080,
                         'CidrIpv4': '0.0.0.0/0',
-                        'Description': 'Tech-hub template '
+                        'Description': DESCRIPTION
                     }
                 },
             ])
 
     except ClientError:
-        logger.exception('This Could not modify security group rule.')
+        logger_for.exception('This Could not modify security group rule.')
         raise
     else:
-        return response
+        return res
 
 
 if __name__ == '__main__':
-    SECURITY_GROUP_ID = input("Please enter the Security group ID")
-    SECURITY_GROUP_RULE_ID = input("Please enter the Security group Rule ID")
-    # SECURITY_GROUP_ID = 'sg-022ed25b68ad24c18'
-    # SECURITY_GROUP_RULE_ID = 'sgr-02204706ef3300225'
-    logger.info(f'Please wait, We are modifing your security group rule...')
-    rule = rule_modification(SECURITY_GROUP_ID, SECURITY_GROUP_RULE_ID)
-    logger.info(
-        f'Wow!! your Security group rule has been modified: \n{json.dumps(rule, indent=4)}')
+    GRP_ID = input("Please enter the Security group ID: ")
+    GRP_RULE_ID = input("Please enter the Security group Rule ID: ")
+    DESCRIPTION = input("Enter the description for modification: ")
+    logger_for.info(f'Please wait, We are modifing your security group rule...')
+    result = rule_modification(GRP_ID, GRP_RULE_ID)
+    logger_for.info(
+        f'Wow!! your Security group rule has been modified: \n{json.dumps(result, indent=4)}')
